7d6d52dd1d47e3af8146d195894512ee4edab249
Rafał Grzywacz <rafalgrzywacz1994@gmail.com>
2017-06-05 20:30:22 +0200

    Dodanie insertów do News i Comment, dodanie zapytań, dodanie 3 indeksów

--- ProjectX 2017-06-05.sql
+++ ProjectX 2017-06-05.sql
@@ -1,3 +1,5 @@
+use ProjectX;
+
 CREATE Table UserTable
 (
 id int identity(1,1) primary key,
@@ -69,13 +71,14 @@ readerId int not null
 CONSTRAINT FK_READER_READERRANK FOREIGN KEY (readerId) REFERENCES Reader(id)
 )
 
-DECLARE @iterator INT = 0;
 
+DECLARE @iterator INT = 0;
 WHILE @iterator < 50
 BEGIN
 	insert into UserTable(login,password, firstName, lastName)  values ('user' + CONVERT(Varchar(2),@iterator) + '@gmail.com' ,'userPassword1234','user' + CONVERT(Varchar(2),@iterator),'lastname' + CONVERT(Varchar(2),@iterator));
    SET @iterator = @iterator + 1;
 END;
+
 SET @iterator = 0;
 WHILE @iterator < 50
 BEGIN
@@ -100,32 +103,53 @@ BEGIN
 	values ('Category' + CONVERT(Varchar(2),@iterator),1);
    SET @iterator = @iterator + 1;
 END;
+
 SET @iterator = 0;
 WHILE @iterator < 500
 BEGIN
 	insert into ProjectX.dbo.News (mainTitle,subtitle,content,banner,writerId,categoryId)
 	values ('MainTitle' + CONVERT(Varchar(3),@iterator),'SubTitle' + CONVERT(Varchar(3),@iterator),'Content' + CONVERT(Varchar(3),@iterator),1011,
-	(select ID from ProjectX.dbo.Writer where nickName='Writer' + CONVERT(Varchar(10),ABS(Checksum(NewID()) % 23) + 1)),
+	(select ID from ProjectX.dbo.Writer where nickName='Writer' + CONVERT(Varchar(10),(@iterator% 23)+1)),
 	ABS(Checksum(NewID()) % 24) + 1);
 	SET @iterator = @iterator + 1;
 END;
 
-
-use ProjectX;
+SET @iterator = 0;
+WHILE @iterator < 500
+BEGIN 
+	insert into ProjectX.dbo.Comment (content,readerId,newsId)
+	values ('ConttentComment' +CONVERT(Varchar(3),@iterator),
+		   (select ID from ProjectX.dbo.Reader where nickName='Reader' + CONVERT(Varchar(10),(@iterator% 25)+25)),
+		   (select ID from ProjectX.dbo.News where mainTitle='MainTitle' + CONVERT(Varchar(10),@iterator)));
+SET @iterator = @iterator + 1;
+END;
 
 ----=======================================3 Zapytania============================================--
+SELECT mainTitle,writerId FROM ProjectX.dbo.News 
+WHERE writerId = (SELECT ID FROM ProjectX.dbo.UserTable WHERE LOGIN='user'+CONVERT(Varchar(2),CAST(25*RAND() AS int) + 1)+'@gmail.com')
+GROUP BY mainTitle, writerId
+HAVING LEN(mainTitle)>1;
 
---select mainTitle,writerId from ProjectX.dbo.News 
---where writerId = (select ID from ProjectX.dbo.UserTable where login='Rafss@gmail.com')
---group by mainTitle, writerId
---having LEN(mainTitle)>1;
+SELECT n.mainTitle AS 'Tytu³' ,w.nickName AS 'Nick pisarza',n.writerId AS 'Idendyfikator pisarza' FROM ProjectX.dbo.News n
+LEFT JOIN ProjectX.dbo.Writer w ON n.writerId = w.id
+WHERE writerId = (SELECT ID FROM ProjectX.dbo.UserTable WHERE LOGIN='user'+CONVERT(Varchar(2),CAST(25*RAND() AS int) + 1)+'@gmail.com')
+GROUP BY n.mainTitle, n.writerId, w.nickName
+HAVING LEN(n.mainTitle)>1;
+
+SELECT AVG(cnt) AS 'Œrednia liczba komentarzy czytelników'
+FROM (SELECT readerId, COUNT(*) AS cnt
+      FROM ProjectX.dbo.Comment  s 
+      GROUP BY readerId
+      ) t;
 
 ----==================================4 funckje i procedury=======================================--
 
 
 ----========================================5 indeksy=============================================--
-----nieklastrowe
---create unique nonclustered index UserTable_I_1 on ProjectX.dbo.UserTable(login);
+CREATE UNIQUE NONCLUSTERED INDEX UserTable_I_1 ON ProjectX.dbo.UserTable(login);
+CREATE UNIQUE NONCLUSTERED INDEX Writer_I_1 ON ProjectX.dbo.Writer(userId);
+CREATE UNIQUE NONCLUSTERED INDEX Reader_I_1 ON ProjectX.dbo.Reader(userId);
+
 ----========================================6 widoki==============================================--
 --use ProjectX;
 --CREATE VIEW UserNews AS
